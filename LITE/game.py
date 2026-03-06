0f574a9507de206e8456a4a66e7f4ee611cfd9cf
Gleb <glebgaevoy1@gmail.com>
2026-03-06 08:58:23 +0300

    basic game class and main file implemented

---
+++ LITE/game.py
@@ -0,0 +1,24 @@
+import pygame
+
+class Game():
+    def __init__(self, screen_width=1000, screen_height=1000):
+        pygame.init()
+        self.running = True
+        self.screen_width, self.screen_height = screen_width, screen_height
+        self.display = pygame.Surface((self.screen_width, self.screen_height))
+        self.window = pygame.display.set_mode((self.screen_width, self.screen_height))
+        self.font_name = 'VCR_OSD_MONO.ttf'
+
+        self.BLACK, self.WHITE, self.BLUE = (0, 0, 0), (255, 255, 255), (0, 0, 255)
+
+    def game_loop(self):
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_q: #Q - quit
+                        self.running = False
+                    if event.key == pygame.K_SPACE: #SPACE - next
+                        pass # switch to next
+
---
+++ LITE/main.py
@@ -0,0 +1,8 @@
+import pygame
+from LITE.game import Game
+
+g = Game()
+
+while g.running:
+    g.game_loop()
+
